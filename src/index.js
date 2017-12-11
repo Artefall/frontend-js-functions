5f6e2e7b84b7141aff87a7d24463cf0a12b673de
Artefall <32461472+Artefall@users.noreply.github.com>
2017-12-11 23:56:54 +0200

    solution "sum'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-﻿console.log(2)
+﻿function min(arr){
+	if (arr.length == undefined || arr.length == null){
+		console.log("Массив пуст!");
+	}
+
+	var min = arr.filter(function(argument){
+		return isBig(argument);
+	})
+	console.log(min);
+}
+
+function max(arr){
+	var max;
+	if (arr.length == undefined || arr.undefined == null){
+		console.log("Массив пуст!");
+	}
+}
+
+function sum(arr){
+	if (arr.length === undefined || arr.lenght === null){
+		console.log("Массив пуст!");
+	}
+
+	var result = arr.reduce(function(sum, current) {
+		return sum + current;
+	  }, 0);
+	
+	console.log(result)
+}
