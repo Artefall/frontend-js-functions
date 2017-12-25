c057cb290c4cf90b13d4bb4fe853c75d9906e611
Artefall <32461472+Artefall@users.noreply.github.com>
2017-12-25 21:32:37 +0200

    Functions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-﻿function min(arr){
-	if (arr.length == undefined || arr.length == null){
-		console.log("Массив пуст!");
-	}
+﻿lore = [12,58,278,50,65,92,64,753,85]
 
-	var min = arr.filter(function(argument){
-		return isBig(argument);
-	})
-	console.log(min);
-}
+function min(arr) {
+    return arr.reduce(function (previousVariable, newVariable) {
+      return ( previousVariable < newVariable ? previousVariable : newVariable );
+    });
+  }
+  
+  function max(arr) {
+    return arr.reduce(function (previousVariable, newVariable) {
+      return ( previousVariable > newVariable ? previousVariable : newVariable );
+    });
+  }
 
-function max(arr){
-	var max;
-	if (arr.length == undefined || arr.undefined == null){
-		console.log("Массив пуст!");
-	}
-}
-
-function sum(arr){
-	if (arr.length === undefined || arr.lenght === null){
-		console.log("Массив пуст!");
-	}
-
-	var result = arr.reduce(function(sum, current) {
-		return sum + current;
-	  }, 0);
-	
-	console.log(result)
-}
+  function sum(arr){
+    var result = arr.reduce(function(sum, current) {
+        return sum + current;
+      }, 0);
+  }
+  
+console.log(max(lore))
+console.log(min(lore))
+console.log(sum(lore))
